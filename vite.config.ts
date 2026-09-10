9a1ae1265ddae802ca7ac093349b26c2a1691b32
zzzinyy <ziny967@gmail.com>
2026-09-10 19:13:07 +0200

    Configure Vite with React and Nitro plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,14 @@
 ```ts
-export default {};
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { defineConfig } from "vite";
+import { nitro } from "nitro/vite";
+import viteReact from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [
+    tanstackStart(),
+    nitro(),
+    viteReact(),
+  ],
+});
 ```
