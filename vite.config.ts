5f29a40cfab3bf9dd9b5dac9f338419ea1d7e767
Zin <ziny967@gmail.com>
2026-09-12 10:40:33 +0200

    Corrige configuracion de Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,11 @@
-```ts
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import { defineConfig } from "vite";
-import { nitro } from "nitro/vite";
 import viteReact from "@vitejs/plugin-react";
 
 export default defineConfig({
   base: "/GadgetMatrix/",
   plugins: [
     tanstackStart(),
-    nitro(),
     viteReact(),
   ],
 });
-```
