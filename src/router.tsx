ecb6941ed364bde3fb1872752e58caad1b998883
Zin <ziny967@gmail.com>
2026-09-12 10:56:37 +0200

    Corrige basepath del router

--- src/router.tsx
+++ src/router.tsx
@@ -7,7 +7,6 @@ export const getRouter = () => {
 
   const router = createRouter({
     routeTree,
-    basepath: "/GadgetMatrix",
     context: { queryClient },
     scrollRestoration: true,
     defaultPreloadStaleTime: 0,
