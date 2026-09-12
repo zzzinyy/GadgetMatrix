002ebb3f044debecf7ae3fffb5224128c04ae0d8
Zin <ziny967@gmail.com>
2026-09-12 11:13:45 +0200

    Corrige redirecciones de autenticacion

--- src/routes/auth.tsx
+++ src/routes/auth.tsx
@@ -81,7 +81,7 @@ function AuthPage() {
           ...parsed.data,
           options: {
             emailRedirectTo:
-              "https://zzinny.github.io/GadgetMatrix/admin",
+              "https://zzzinyy.github.io/GadgetMatrix/admin",
           },
         });
 
@@ -105,7 +105,7 @@ function AuthPage() {
 
     try {
       const redirectTo =
-        "https://zzinny.github.io/GadgetMatrix/admin";
+        "https://zzzinyy.github.io/GadgetMatrix/admin";
 
       const { data, error } = await supabase.auth.signInWithOAuth({
         provider: "google",
